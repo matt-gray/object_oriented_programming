7ab52a20b4685208d478195309d9be8c1002376c
Matt Gray <matt@mattgray.ca>
2013-10-24 12:06:11 -0400

    part 7

--- people.rb
+++ people.rb
@@ -6,12 +6,22 @@ class Person
   end
 end
 
-class Student
+class Student < Person
   def learn
     puts "I get it!"
+  end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object"
+  end
 end
+
+instructor = Instructor.new
+instructor.name = "Chris"
+puts instructor.greeting
+
+student = Student.new
+student.name = "Cristina"
+puts student.greeting
