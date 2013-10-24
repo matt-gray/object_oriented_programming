13a596b203031abb9bb460880667b7b00bed24a9
Matt Gray <matt@mattgray.ca>
2013-10-24 12:10:33 -0400

    part 8

--- people.rb
+++ people.rb
@@ -18,10 +18,33 @@ class Instructor < Person
   end
 end
 
+#part 6
 instructor = Instructor.new
 instructor.name = "Chris"
 puts instructor.greeting
 
+#part 7
 student = Student.new
 student.name = "Cristina"
 puts student.greeting
+
+#part 8
+instructor.teach
+student.learn
+
+instructor.learn
+student.teach
+#produces erro => e
+
+=begin
+  people.rb:35:in `<main>': undefined method `learn' for 
+  #<Instructor:0x007fda438244f0 @name="Chris"> (NoMethodError)
+  This produces an error because the learn method does not exist
+  within the instructor class. 
+=end
+
+
+
+
+
+
